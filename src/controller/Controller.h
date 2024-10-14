a07b31a432383f3c7d7ec63ca5cadcc85fbbb9a2
SoshiroFujimori <oshima.k.kondate@gmail.com>
2024-10-14 12:09:46 +0900

    NUM_BUTTONSをNUM_OTHER_BUTTONSに変更し、コントローラーのボタン定義を明確化

--- src/controller/Controller.h
+++ src/controller/Controller.h
@@ -5,9 +5,9 @@
 #include <liboshima.h>
 
 // モーターの数を定義
-#define NUM_MOTORS 1  // モーターは1つ
-#define NUM_BUTTONS 0 // モーター以外にボタンは使用しない
-#define NUM_STICKS 0  // スティックは使用しない
+#define NUM_MOTORS 1        // モーターは1つ
+#define NUM_OTHER_BUTTONS 0 // モーター以外にボタンは使用しない
+#define NUM_STICKS 0        // スティックは使用しない
 
 // Controller型をControllerData型として扱えるようにする
-using Controller = ControllerData<NUM_MOTORS, NUM_BUTTONS, NUM_STICKS>;
+using Controller = ControllerData<NUM_MOTORS, NUM_OTHER_BUTTONS, NUM_STICKS>;
